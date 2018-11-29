ef53fdfe3b359bdc7ba34cf5fd650192c3357a36
Junying Li <jesselix@live.com>
2018-11-29 22:58:33 +0800

    =

--- mall/src/test/java/li/jesse/mall/MallApplicationTests.java
+++ mall/src/test/java/li/jesse/mall/MallApplicationTests.java
@@ -1,4 +1,4 @@
-package li.jesse.mallbackend;
+package li.jesse.mall;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MallBackendApplicationTests {
+public class MallApplicationTests {
 
     @Test
     public void contextLoads() {
