8978d70b29336c48c4da6065cfee31c8c87fc86e
Junying Li <jesselix@live.com>
2019-02-14 15:16:49 +0800

    +

--- account-center/src/test/resources/sql/db.sql
+++ account-center/src/test/resources/sql/db.sql
@@ -1,4 +1,4 @@
 
-create database if not exists account_center default charset utf8 collate utf8_general_ci;
+create database if not exists account_center default charset utf8 collate utf8_unicode_ci;
 
 use account_center;
